1422554f4d6b1072ec3f578c52a10e1a6c2612fc
AlejandroUnahur <v.alejandrohugo@gmail.com>
2021-05-31 23:15:32 -0300

    Creada la clase tarea y sus respectivos test

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -1,3 +1,28 @@
 package ar.edu.unahur.obj2.tareas
 
-class Tarea
+abstract class Tarea(val horasEstimadas: Int, val responsable: Empleado  ) {
+
+    abstract fun nominaEmpleados(): List<Empleado>
+
+    abstract fun horasParaFinalizarTarea():Int
+
+    abstract fun costo(): Int
+
+ }
+
+class TareaSimple(val costoInfraestructura: Int,val empleados: List<Empleado>, horasEstimadas: Int, responsable: Empleado) : Tarea(horasEstimadas, responsable) {
+
+    override fun nominaEmpleados() = this.empleados + this.responsable
+
+    override fun horasParaFinalizarTarea() = this.horasEstimadas + this.cantidadEmpleados()
+
+    override fun costo() = this.costoEmpleados() + this.costoResponsable() + this.costoInfraestructura
+
+    fun cantidadEmpleados() = this.empleados.size
+
+    fun costoEmpleados() = (this.horasEstimadas / this.cantidadEmpleados()) * this.sueldoPorHoraEmpleados()
+
+    fun sueldoPorHoraEmpleados() = this.empleados.sumBy { it.costoPorHora }
+
+    fun costoResponsable() = this.horasEstimadas * this.responsable.costoPorHora
+}
--- src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
@@ -1,8 +1,25 @@
 package ar.edu.unahur.obj2.tareas
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 
-class TareaTest : DescribeSpec({
-  describe("Una tarea") {
-  }
+ class TareaTest : DescribeSpec({
+    describe("Una tarea") {
+      val empleadoResponsable = Empleado(1000)
+      val empleado1 = Empleado(500)
+      val empleado2 = Empleado(500)
+      val tarea = TareaSimple(1000, listOf(empleado1,empleado2),50,empleadoResponsable)
+
+      it("Nomina de empleados que es una lista con los empleados y el responsable") {
+        tarea.nominaEmpleados().shouldBe(listOf(empleado1,empleado2,empleadoResponsable))
+      }
+
+      it("Horas que se necesitan para finalizar que es la suma de las horas estimadas mas la cantidad de empleados ") {
+        tarea.horasParaFinalizarTarea().shouldBe(52)
+      }
+
+      it("El costo que es la suma del costo de los empleados, el costo del responsable y el costo de la infraestructura") {
+        tarea.costo().shouldBe(76000)
+      }
+    }
 })
