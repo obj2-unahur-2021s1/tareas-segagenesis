31845522088e7581df8b62f0ead81f2426545fad
AlejandroUnahur <v.alejandrohugo@gmail.com>
2021-06-01 03:25:46 -0300

    Creada la clase tarea de integracion y sus respectivos test

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -1,20 +1,19 @@
 package ar.edu.unahur.obj2.tareas
 
-abstract class Tarea(val horasEstimadas: Int, val responsable: Empleado  ) {
+abstract class Tarea(val responsable: Empleado  ) {
 
     abstract fun nominaEmpleados(): List<Empleado>
 
     abstract fun horasParaFinalizarTarea():Int
 
     abstract fun costo(): Int
-
  }
 
-class TareaSimple(val costoInfraestructura: Int,val empleados: List<Empleado>, horasEstimadas: Int, responsable: Empleado) : Tarea(horasEstimadas, responsable) {
+class TareaSimple(val horasEstimadas: Int, val costoInfraestructura: Int,val empleados: List<Empleado>, responsable: Empleado) : Tarea( responsable) {
 
     override fun nominaEmpleados() = this.empleados + this.responsable
 
-    override fun horasParaFinalizarTarea() = this.horasEstimadas + this.cantidadEmpleados()
+    override fun horasParaFinalizarTarea() = this.horasEstimadas / this.cantidadEmpleados()
 
     override fun costo() = this.costoEmpleados() + this.costoResponsable() + this.costoInfraestructura
 
@@ -25,4 +24,21 @@ class TareaSimple(val costoInfraestructura: Int,val empleados: List<Empleado>, h
     fun sueldoPorHoraEmpleados() = this.empleados.sumBy { it.costoPorHora }
 
     fun costoResponsable() = this.horasEstimadas * this.responsable.costoPorHora
+}
+
+class TareaIntegracion(responsable: Empleado, val subtareas: List<TareaSimple>) : Tarea(responsable) {
+
+    override fun nominaEmpleados() = this.nominaSubtareas() + this.responsable
+
+    override fun horasParaFinalizarTarea() =  this.horasParaFinalizarSubtareas() + this.tiempoPlanificacion()
+
+    override fun costo() = this.costoTareasACoordinar() + (this.costoTareasACoordinar() * 3 / 100)
+
+    fun horasParaFinalizarSubtareas() = this.subtareas.sumBy { it.horasParaFinalizarTarea() }
+
+    fun nominaSubtareas() = this.subtareas.flatMap { it.nominaEmpleados()  }
+
+    fun tiempoPlanificacion() = this.horasParaFinalizarSubtareas() / 8
+
+    fun costoTareasACoordinar() = this.subtareas.sumBy { it.costo() }
 }
--- src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
@@ -4,22 +4,47 @@ import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
  class TareaTest : DescribeSpec({
+
     describe("Una tarea") {
-      val empleadoResponsable = Empleado(1000)
+      val responsable = Empleado(1000)
       val empleado1 = Empleado(500)
       val empleado2 = Empleado(500)
-      val tarea = TareaSimple(1000, listOf(empleado1,empleado2),50,empleadoResponsable)
+      val tarea = TareaSimple(50, 1000,listOf(empleado1,empleado2),responsable)
+
 
       it("Nomina de empleados que es una lista con los empleados y el responsable") {
-        tarea.nominaEmpleados().shouldBe(listOf(empleado1,empleado2,empleadoResponsable))
+        tarea.nominaEmpleados().shouldBe(listOf(empleado1,empleado2,responsable))
       }
 
-      it("Horas que se necesitan para finalizar que es la suma de las horas estimadas mas la cantidad de empleados ") {
-        tarea.horasParaFinalizarTarea().shouldBe(52)
+      it("Horas que se necesitan para finalizar que es la suma de las horas estimadas dividido la cantidad de empleados ") {
+        tarea.horasParaFinalizarTarea().shouldBe(25)
       }
 
       it("El costo que es la suma del costo de los empleados, el costo del responsable y el costo de la infraestructura") {
         tarea.costo().shouldBe(76000)
       }
     }
+
+   describe("Una tarea de integracion") {
+     val empleado1 = Empleado(500)
+     val empleado2 = Empleado(500)
+     val responsable1 = Empleado(1000)
+     val responsable2 = Empleado(1000)
+     val responsableIntegracion = Empleado(1000)
+     val tarea1 = TareaSimple(50,1000, listOf(empleado1),responsable1)
+     val tarea2 = TareaSimple(50,1000, listOf(empleado2),responsable2)
+     val tareaIntegracion = TareaIntegracion(responsableIntegracion, listOf(tarea1,tarea2))
+
+     it("Nomina de empleados que es la suma de las nóminas de las subtareas más el responsable de la tarea de integración. "){
+       tareaIntegracion.nominaEmpleados().shouldBe(listOf(empleado1,responsable1,empleado2,responsable2,responsableIntegracion))
+     }
+
+     it("Las horas necesarias para realizarla se calculan como la suma de lo que tardan sus subtareas más una hora para reuniones de planificación por cada 8 horas de trabajo real") {
+       tareaIntegracion.horasParaFinalizarTarea().shouldBe(112)
+     }
+
+     it("El costo es la suma de los costos de sus subtareas más un bonus que se le paga al responsable, equivalente al 3% de esa suma.") {
+       tareaIntegracion.costo().shouldBe(156560)
+     }
+   }
 })
